cecec3935f05528a29ae2757b696157f43c06b85
blackwhite2018 <39052924+blackwhite2018@users.noreply.github.com>
2020-06-14 23:05:00 +0300

    Updates

---
+++ precache-manifest.9320d4709654b8a22b227196475c79c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae0a6e1ea426f5b7e39f2972ed6f9f5",
+    "url": "/bootstrap-card/index.html"
+  },
+  {
+    "revision": "9462e78a45267d2bae1e",
+    "url": "/bootstrap-card/static/css/main.0976046a.chunk.css"
+  },
+  {
+    "revision": "85aeef93d914c59d6761",
+    "url": "/bootstrap-card/static/js/2.d9b275c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bootstrap-card/static/js/2.d9b275c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9462e78a45267d2bae1e",
+    "url": "/bootstrap-card/static/js/main.1bccfe1a.chunk.js"
+  },
+  {
+    "revision": "c3c073e7da7413ad4591",
+    "url": "/bootstrap-card/static/js/runtime-main.93542d3c.js"
+  }
+]);
